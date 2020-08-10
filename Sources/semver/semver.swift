8e07eee928bc9b9d2037d3d2f5beccb0a9f6722e
Gwynne Raskind <gwynne@darkrainfall.org>
2020-08-10 07:23:40 -0500

    Initial setup and commit locally

--- Sources/semver/semver.swift
+++
@@ -1,3 +0,0 @@
-struct semver {
-    var text = "Hello, World!"
-}
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import semverTests
-
-var tests = [XCTestCaseEntry]()
-tests += semverTests.allTests()
-XCTMain(tests)
