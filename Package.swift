eed03e69e8e04c3daf67cebd3d9be3a1b0835471
Gwynne Raskind <gwynne@darkrainfall.org>
2023-09-11 17:43:10 -0500

    Bump dependency version

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
-        .package(url: "https://github.com/gwynne/swift-semver.git", from: "0.2.0-alpha"),
+        .package(url: "https://github.com/gwynne/swift-semver.git", from: "1.0.0-beta"),
     ],
     targets: [
         .executableTarget(name: "semver", dependencies: [
